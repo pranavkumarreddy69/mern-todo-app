cbc2690404649cd0b979c9be016fe1f9f31956de
pranavkumarreddy69 <pranavkumarreddy69@gmail.com>
2026-05-24 17:28:00 +0530

    fixed render port

--- server/server.js
+++ server/server.js
@@ -123,6 +123,10 @@ app.post("/login", async (req, res) => {
 
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
